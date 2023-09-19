aa70b84d4ce73d73a9f91b19a8d2298d233ccc83
user-prateek <prateek.n@maventechlabs.com>
2023-09-19 18:23:51 +0530

    Updates

---
+++ precache-manifest.3359ed5ee25c73fe210be7e3e4c98c4d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d9a042d1208b2bc0e77b93d760a63f",
+    "url": "/portfolio-mui/index.html"
+  },
+  {
+    "revision": "f2ef0da371a438f873e4",
+    "url": "/portfolio-mui/static/css/main.55e7d135.chunk.css"
+  },
+  {
+    "revision": "a902ba07c3c4292efc58",
+    "url": "/portfolio-mui/static/js/2.d2d2e6ed.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/portfolio-mui/static/js/2.d2d2e6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ef0da371a438f873e4",
+    "url": "/portfolio-mui/static/js/main.1b2b0e1c.chunk.js"
+  },
+  {
+    "revision": "611cf3721399de890fac",
+    "url": "/portfolio-mui/static/js/runtime-main.8a81a224.js"
+  },
+  {
+    "revision": "5eb9911d26c96ad521cd249652f47245",
+    "url": "/portfolio-mui/static/media/avatar.5eb9911d.png"
+  },
+  {
+    "revision": "da0508b1d39eb338b4eee04cc4e7149f",
+    "url": "/portfolio-mui/static/media/codeigniter.da0508b1.png"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio-mui/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
